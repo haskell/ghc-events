a4c538ed64e9c83304e4e307cab5aac50ba80722
Spencer Janssen <spencer@well-typed.com>
2011-03-28 15:24:00 +0000

    Finish test suite

--- test/TestVersions.hs
+++ test/TestVersions.hs
@@ -1,16 +1,22 @@
-import GHC.RTS.Events
-import System.Exit
-
 {-
-TODO: perform a sanity check on the deserialized logs.  Fuzzy matching may be
-required.  For now, we simply check whether deserialization is successful.
+This test parses sample event logs from each major GHC version and compares
+the pretty printed output with reference output.
 
-Steps to produce event log:
+When tests fail, use a diff tool to compare the output of show-ghc-events with
+the reference file.  Resolve the differences in either the library code or the
+reference file, and 'darcs record' the changes.
+
+Steps to produce event log and reference output:
     $ ghc --make queens.hs -o queens-ghc-$VERSION -threaded -eventlog
     $ queens-ghc-$VERSION 8 +RTS -N4 -ls
+    $ show-ghc-events queens-ghc-$VERSION.eventlog > queens-ghc-$VERSION.eventlog.reference
 
 Where queens.hs is http://darcs.haskell.org/nofib/parallel/queens/Main.hs
 -}
+
+import GHC.RTS.Events
+import System.Exit
+
 files :: [FilePath]
 files = map ("test/"++)
     [ "queens-ghc-6.12.1.eventlog"
@@ -20,11 +26,16 @@ files = map ("test/"++)
 testFile :: FilePath -> IO Bool
 testFile f = do
     e <- readEventLogFromFile f
+    let oops s = putStrLn (f ++ ": failure" ++ s) >> return False
+
     case e of
-        Right _ -> putStrLn (f ++ ": success") >> return True
-        Left m  -> putStrLn (f ++ ": fail; " ++ m) >> return False
+        Left m -> oops m
 
-
+        Right newlog -> do
+            oldlog <- readFile (f ++ ".reference")
+            if oldlog == ppEventLog newlog
+                then putStrLn (f ++ ": success") >> return True
+                else oops "pretty print output does not match"
 main = do
     successes <- mapM testFile files
     if and successes
