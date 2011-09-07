bc9828975ddc94221fd949c2831a5aa742412fae
Mikolaj Konarski <mikolaj@well-typed.com>
2011-09-07 10:25:11 +0000

    change mentions of "show-ghc-events" to "ghc-events"

--- test/TestVersions.hs
+++ test/TestVersions.hs
@@ -2,14 +2,14 @@
 This test parses sample event logs from each major GHC version and compares
 the pretty printed output with reference output.
 
-When tests fail, use a diff tool to compare the output of show-ghc-events with
+When tests fail, use a diff tool to compare the output of "ghc-events show" with
 the reference file.  Resolve the differences in either the library code or the
 reference file, and 'darcs record' the changes.
 
 Steps to produce event log and reference output:
     $ ghc --make queens.hs -o queens-ghc-$VERSION -threaded -eventlog
     $ queens-ghc-$VERSION 8 +RTS -N4 -ls
-    $ show-ghc-events queens-ghc-$VERSION.eventlog > queens-ghc-$VERSION.eventlog.reference
+    $ ghc-events show queens-ghc-$VERSION.eventlog > queens-ghc-$VERSION.eventlog.reference
 
 Where queens.hs is http://darcs.haskell.org/nofib/parallel/queens/Main.hs
 -}
